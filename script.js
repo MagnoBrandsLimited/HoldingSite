067a3dbdcc60bbec63358cce4461a201b55f440c
MagnoBrandsLimited <henry@magnobrands.com>
2024-11-02 14:47:08 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function notifyMe() {
-    const email = document.getElementById("email").value;
-    if (email) {
-        alert('Thanks for signing up! You will be notified when the app launches.');
-    } else {
-        alert('Please enter a valid email address.');
-    }
-}
