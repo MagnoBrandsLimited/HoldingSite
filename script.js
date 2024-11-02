233d18d0712deb48125f9faba867a4a4cba42884
MagnoBrandsLimited <henry@magnobrands.com>
2024-11-02 14:27:41 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Code (script.js)
+const jsContent = `
+function notifyMe() {
+    const email = document.getElementById("email").value;
+    if (email) {
+        alert('Thanks for signing up! You will be notified when the app launches.');
+    } else {
+        alert('Please enter a valid email address.');
+    }
+}
+`;
+
+// Here you can serve or save these contents as files to build the complete "Coming Soon" webpage.
+console.log(htmlContent);
+console.log(cssContent);
+console.log(jsContent);
